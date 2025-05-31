40e435ef1082460c1e7eab5315b5ec6026247199
Aleksei <aleksei_korobov@mail.ru>
2025-05-31 18:42:37 +0300

    update sorting

--- files/moving_file/sort_by_ex.py
+++ files/moving_file/sort_by_ex.py
@@ -1,7 +1,18 @@
+#
+#uv run --directory /media/aleksei/home/MyProject/python/files moving_file/sort_by_ex.py
+#
+
+
+from loguru import logger
 import os
 
+if not os.path.isfile("log/sort_by_ex.log"):
+    raise(Exception('not file path'))
+
+logger.add("log/sort_by_ex.log", enqueue=True, rotation="500 MB",level='INFO')
+
 categories = {
-    'docs':'.docx,.doc,.pdf,.djvu,.txt,.mhtml,.htm,.html,.xps,.DOCX,.pptx,.md,.rtf,.mht',
+    'docs':'.docx,.doc,.pdf,.djvu,.txt,.mhtml,.htm,.html,.xps,.DOCX,.pptx,.md,.rtf,.mht,.fb2',
     'excel':'.xls,.xlsx,.csv,.xltx,.xlsm,.xlt',
     'Diagram':'.vsdx,.drawio,.eapx',
     'log':'.log',
@@ -22,8 +33,8 @@ categories = {
 }
 
 base_dirs = [
-    '/home/aleksei/Download',
-    #'/home/aleksei/Загрузки/Other/'
+    '/home/aleksei/Downloads',
+    '/home/aleksei/Downloads/Telegram Desktop/'
     #r'c:\Users\akorobov\Documents',
     #r'c:\Users\akorobov\Downloads',
     #r'c:\Users\akorobov\Desktop',
@@ -34,50 +45,52 @@ def move_file(base_dir,file_name,key):
     file_path = os.path.join(base_dir,file_name)
 
     if not os.path.exists(file_path):        
-        print(f'file not exists {file_path}')
+        logger.warning(f'file not exists {file_path}')
         return
     file_new_path = os.path.join(base_dir,key,file_name)
     if len(file_new_path) > 255:
-        print(f'file {file_name} is very long')
+        logger.warning(f'file {file_name} is very long')
         return
 
-    #print(f'file new path {file_new_path}')
+    #logger.debug(f'file new path {file_new_path}')
     index = 1
     while os.path.exists(file_new_path):
-        print(f'file exists {file_new_path}')
+        logger.debug(f'file exists {file_new_path}')
         filename, file_extension = os.path.splitext(file_name)
         file_name_new = f'{filename} ({index}){file_extension}'
         file_new_path = os.path.join(base_dir,key,file_name_new)
         index += 1
-    print(f'move {file_path} -> {file_new_path}')
+    logger.info(f'move {file_path} -> {file_new_path}')
     os.rename(file_path,file_new_path)
 
 
 def sort_by_cat(base_dir,key):
     val = categories[key]
-    print(f'{key} - {val}')
+    logger.debug(f'{key} - {val}')
     full_dir = os.path.join(base_dir,key)
     vals = val.split(',')
-    print(f'vals - {tuple(vals)}')
+    logger.debug(f'vals - {tuple(vals)}')
     files = os.listdir(base_dir)
 
-    print(f'all files - {len(files)}')
+    logger.debug(f'all files - {len(files)}')
 
     if not 'Other' == key:        
         files = [ file for file in files if file.lower().endswith( tuple(vals) ) ]
     else:
         files = [ file for file in files if os.path.isfile(os.path.join(base_dir,file)) ]
 
-    print(f'filtered files - {len(files)}')
+    logger.debug(f'filtered files - {len(files)}')
     if len(files)>0:
         if not os.path.exists(full_dir):
-            print(f'create dir - {full_dir}')
+            logger.debug(f'create dir - {full_dir}')
             os.mkdir(full_dir)
         for v in files:        
             move_file(base_dir,v,key)
 
-for base_dir in base_dirs:
-    print(f'base_dir - {base_dir}')
-    for cat in categories:
-        sort_by_cat(base_dir,cat)
-        print(cat)
+if __name__ == '__main__':
+    logger.info('start sorting')
+    for base_dir in base_dirs:
+        logger.debug(f'base_dir - {base_dir}')
+        for cat in categories:
+            sort_by_cat(base_dir,cat)
+            logger.debug(cat)
---
+++ files/run_sort_by_ex.sh
@@ -0,0 +1,1 @@
+uv run --directory /media/aleksei/home/MyProject/python/files moving_file/sort_by_ex.py
